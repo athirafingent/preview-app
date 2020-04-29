164bb4b3fbc8bb2e8d8c58a687b3b4c1b6918fb4
Athira soman <athira.soman@fingent.com>
2020-04-29 10:41:02 +0530

    Pdf download protection in ngx-doc-viewer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent {
   title = 'preview-app';
   url:string;
+  pdf_protected:number;
 
   previewPdf() {
+    this.pdf_protected = 0;
     this.url = 'http://www.africau.edu/images/default/sample.pdf';
     // this.url = 'https://www.montclair.edu/media/montclairedu/oit/documentation/whatsnewinoffice20072003/Understanding_Views.pdf';
   }
   previewDoc() {
+    this.pdf_protected = 0;
+    this.url = 'https://file-examples.com/wp-content/uploads/2017/02/file-sample_100kB.docx';
+  }
+  previewPdfProtected() {
+    this.pdf_protected = 1;
     this.url = 'https://file-examples.com/wp-content/uploads/2017/02/file-sample_100kB.docx';
   }
 }
